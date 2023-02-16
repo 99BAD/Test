a82bd959b43159ebbd2ac4b8216c1a8bfc7993be
99BAD <125058781+99BAD@users.noreply.github.com>
2023-02-16 09:53:57 +0100

    Add files via upload

---
+++ Guess_num_comp.py
@@ -0,0 +1,20 @@
+import random
+
+def computer_guess(x):
+    low = 1
+    high = x
+    k =''
+    while k != 'c':
+        guess= random.randint(low,high)
+        k = input(f'{guess} Too high? (H) Too low? (L) Correct? (C)').lower
+        if k == 'l':
+            high = guess-1
+            print(high)
+        elif k =='h':
+            low = guess+1
+            print(low)
+    
+    print(f'valeur trouvée {guess}')
+
+
+computer_guess(20)
